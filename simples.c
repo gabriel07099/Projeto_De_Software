90c2329d47f10fb7bb3267f557751715e13d7964
Gabriel Montes <84678205+fiscal157@users.noreply.github.com>
2022-11-17 16:06:50 -0300

    Update simples.c

--- simples.c
+++ simples.c
@@ -1,16 +1,22 @@
 
 /*importa as bibliotecas a serem usadas e define o numero de threads*/
-#include <stdio.h>
-#include <stdlib.h>
-#include <pthread.h>
+#include <stdio.h> 
+#include <stdlib.h> // permite trablhar com strings
+#include <pthread.h> //permite trabalhar com threads
+
 #define num_threads 100
 
 /*aloca as threads,as contas, e o mutex*/
 pthread_t threads[num_threads];
-int c1=100,c2=100, transfs[num_threads];//transfs serve para identificacao do numero de cada thread
+//transfs serve para identificacao do numero de cada thread
+int c1=100,c2=100, transfs[num_threads];
+
+//semaforo
 pthread_mutex_t mutex;
 
 /*as funcoes a definem o comportamento das thread baseado em seu numero*/
+
+//metodo que a thread impar ira executar
 void *threadPar(void *arg)//threads pares
 {
 	int* i = (int*)arg;	//salva o numero da thread
@@ -23,7 +29,7 @@ void *threadPar(void *arg)//threads pares
 	}
 	pthread_mutex_unlock(&mutex); // libera o mutex
 }
-
+//metodo que a thread impar ira executar
 void *threadImpar(void *arg)//threads impares
 {
 	int* i = (int*)arg;//salva o numero da thread
@@ -37,25 +43,32 @@ void *threadImpar(void *arg)//threads impares
 	pthread_mutex_unlock(&mutex);// libera o mutex
 }
 
+//funcao principal
 int main()
 {
 	int i;
 	pthread_mutex_init(&mutex,NULL);// inicializa o mutex
 	
-	for(i=0;i<num_threads;i++){ // inicializa todas as threads
+	// inicializa todas as threads
+	for(i=0;i<num_threads;i++){ 
 		transfs[i] = i;//salva o numero da thread
-		if(i%2 == 0){//calcula se a thread é par 
+		
+		if(i%2 == 0){//calcula se a thread é par
+			//chamada da funcao thread par
 			pthread_create(&threads[i], NULL, &threadPar , (void *) &transfs[i]);//inicializa threads pares informando seu numero
 		}
 		else{
+			//chamada da funcao thread impar
 			pthread_create(&threads[i], NULL, &threadImpar , (void *) &transfs[i]);//inicializa threads impares informando seu numero
 		}
 	}
-	for(i=0;i<num_threads;i++){//finaliza todas as threads
-		pthread_join(threads[i],NULL);
+	//encerra todas as threads
+	for(i=0;i<num_threads;i++){
+		pthread_join(threads[i],NULL); // junta as thread e as mata uma a uma
 		printf("fim transacao %d\n", i);
 	}
-	pthread_mutex_destroy(&mutex);// encerra o mutex
+	//encerra o mutex
+	pthread_mutex_destroy(&mutex);
 	printf("saldo final c1: %d\nsaldo final c2: %d\n",c1,c2);
 
 	return 0;
